795ea951d0e440f60d42d0ba408fd89bba0acf82
Riccardo-Laurenti <riccardo.laurenti95@gmail.com>
2023-09-12 17:39:44 +0200

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        $this->call([TechnologySeeder::class, TypesSeeder::class, ProjectSeeder::class, UserSeeder::class]);
+    }
+}
---
+++ database/seeders/ProjectSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Faker\Generator;
+use App\Models\Project;
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class ProjectSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Generator $faker): void
+    {
+        for ($i = 1; $i <= 50; $i++) {
+            $project = new Project();
+            $project->title = $faker->text(20);
+            $project->content = $faker->paragraph(15, true);
+            $project->image = $faker->imageUrl(250, 250);
+            $project->save();
+        }
+    }
+}
